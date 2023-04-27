dc398f2b8de1ea1e14eb48f66d74ca9be6325a2e
Luke Runnels <lar9482@utulsa.edu>
2023-04-26 23:48:04 -0500

    Hitbox for the bird

--- entities/bird.py
+++ entities/bird.py
@@ -9,7 +9,7 @@ class bird:
                         pg.image.load('assets/bird1.png'), 
                         (0.1 * screen_size[0], 0.05 * screen_size[1])
                     )
-
+        
         #Physics constants(v_0, g, and delta_time)
         self.initial_y_velocity = -35
         self.delta_time = 0.20
@@ -22,9 +22,29 @@ class bird:
         self.x = 0.25 * screen_size[0]
         self.y = 0.25 * screen_size[1]
 
-    #Calculating y= -(1/2)g^2 + v_y*delta_time + y
+        #Hitbox of the bird, just a rectangle for simplicity
+        self.hitbox = pg.Rect((
+            self.x,
+            self.y,
+            self.image.get_width(),
+            self.image.get_height()
+        ))
+        
+
+    #Calculating y= -(1/2)g^2*delta_time + v_y*delta_time + y
     def update_position(self):
+        
+        #Updating the position of the image itself
         self.y += self.y_velocity * self.delta_time
+
+        #Given the updated y-position, update the position of the bird's hitbox
+        self.hitbox.update(
+            self.x,
+            self.y,
+            self.image.get_width(),
+            self.image.get_height()
+        )
+
         self.y_velocity += self.gravity_constant * self.delta_time
 
     #For a jump, the vertically velocity is set back to the original velocity constant
